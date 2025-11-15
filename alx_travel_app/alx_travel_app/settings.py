9087d06e107a3db49b47c9b7c4eebe6bcf5083e4
youssef <youssefelghamour@gmail.com>
2025-11-16 00:48:14 +0100

    0. Database Modeling and Data Seeding in Django

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'drf_yasg',
-    'alx_travel_app.listings',
+    'listings',
 ]
 
 MIDDLEWARE = [
--- alx_travel_app/listings/apps.py
+++ alx_travel_app/listings/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alx_travel_app.listings'
+    name = 'listings'
